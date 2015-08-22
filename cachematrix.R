41af08b4b2b5a32af3cc05c18d2ff0f717cedc16
arongyuris <aron.gyuris@gmail.com>
2015-08-22 16:08:31 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,17 @@
 ## This function serves to store an invertible matrix(x) in a cache for retreival by the function cacheSolve 
 #  
 # makeCacheMatrix <- function(x = matrix()) {           ## Defines function makeCacheMatrix and sets arguments as variable x which is an invertible matrix(x)
-#       m <- NULL                                       	## Sets the variable m to be clear for storage of an input matrix(x)
-#       set <- function(y) {								## Defines variable set as a function to:
-#                x <<- y                                		## Set the value of matrix(x) to variable y in the parent environment (i.e. function makeCacheMatrix)
-#                m <<- NULL                             		## Resets value of variable m as empty in the parent environment
+#       m <- NULL                                           ## Sets the variable m to be clear for storage of an input matrix(x)
+#       set <- function(y) {					            ## Defines variable set as a function to:
+#                x <<- y                                		  ## Set the value of matrix(x) to variable y in the parent environment (i.e. function makeCacheMatrix)
+#                m <<- NULL                             		  ## Resets value of variable m as empty in the parent environment
 #        }
 #       get <- function() x                             ## Defines variable get as a function to retrieve the value of matrix(x) when the function get is called
 #       setsolve <- function(solve) m <<- solve         ## Defines variable setsolve as a function to use the solve function to set the variable m to the inverse of matrix(x)
 #       getsolve <- function() m                        ## Defines variable getsolve as a function to retreive cached variable m, which has been set as the inverse of matrix(x)
 #               list(set = set, get = get,              		## Defines output "special matrix" as a list of the related functions above to be used as input for the function cacheSolve
-#						setsolve = setsolve,					
-#                       getsolve = getsolve)                    
+#			        setsolve = setsolve,					
+#                   getsolve = getsolve)                    
 # }
 #
 makeCacheMatrix <- function(x = matrix()) {
